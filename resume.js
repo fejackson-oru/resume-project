c930af975828d5308c76b1134eaf0759be21703c
Felicity Jackson <227289098+fejackson-oru@users.noreply.github.com>
2025-09-17 09:18:28 -0500

    Create resume.js

---
+++ resume.js
@@ -0,0 +1,12 @@
+// Define Variables
+let myName = "Felicity";
+let title = "Professor";
+let experience = "Helpdesk, Software Dev, and Teacher";
+let education = "Oral Roberts University";
+let skills = ["Skating", "Programming", "Teaching"];
+
+// Connect Variables to the HTML (ligaments)
+document.getElementById("my-name").textContent = myName;
+document.getElementById("title").textContent = title;
+document.getElementById("experience").textContent = experience;
+document.getElementById("education").textContent = education;
